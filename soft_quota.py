1f9b57831e3e6231dbd10bf442b8d171c54743fa
Ken Carlile <carlilek@janelia.hhmi.org>
2016-02-14 19:06:34 -0500

    Expanded soft_quota.py to amke it check for multiple quotas from a quotas.txt file, write current usage to a csv file, as well as separating out the credentials into a credentials.txt file.

--- soft_quota.py
+++ soft_quota.py
@@ -13,7 +13,6 @@
 
 
 # Import python libraries
-import argparse
 import os
 import sys
 import smtplib
@@ -26,9 +25,37 @@ import qumulo.lib.opts
 import qumulo.lib.request
 import qumulo.rest
 
+# Size Definitions
 KILOBYTE = 1024
 MEGABYTE = 1024 * KILOBYTE
 GIGABYTE = 1024 * MEGABYTE
+TERABYTE = 1024 * GIGABYTE
+
+###############CHANGE THESE SETTINGS AND CREATE THESE FILES FOR YOUR ENVIRONMENT #####################################
+# Email settings
+smtp_server = '10.42.5.23'
+sender = 'nrs@janelia.hhmi.org'
+recipients = ['scicompsys@janelia.hhmi.org', 'saffordt@janelia.hhmi.org']
+
+# Location to write log file and header line for log file
+logfile = '/groups/scicomp/reports/nrs_usage/lab_usage.log'
+header = 'Lab,SpaceUsed'
+storagename = '[NRS]' # for email subject
+
+# Import credentials
+# credentials.txt formatted as single line composed of <host fqdn> <username> <password> <portnumber (default 8000)>
+with open("credentials.txt") as file:
+    line = file.readline()
+    host, user, password, port = line.split() 
+
+# Import quota dictionary from quotas.txt file
+# quotas.txt formatted as one line per quota formatted as <short name> <path on Qumulo storage> <nfs mount path> <quota size in TB>
+quota_dict = {}
+with open("quotas.txt") as file:
+    for line in file:
+        quotaname, storage_path, nfs_path, size = line.split()
+        quota_dict[quotaname] = (storage_path, nfs_path, float(size))
+######################################################################################################################
 
 def login(host, user, passwd, port):
     '''Obtain credentials from the REST server'''
@@ -41,7 +68,7 @@ def login(host, user, passwd, port):
 
         # Provide username and password to retreive authentication tokens
         # used by the credentials object
-        login_results, _ = qumulo.rest.auth.login(\
+        login_results, _ = qumulo.rest.auth.login(
                 conninfo, None, user, passwd)
 
         # Create the credentials object which will be used for
@@ -54,66 +81,58 @@ def login(host, user, passwd, port):
 
     return (conninfo, creds)
 
-def send_mail(smtp_server, sender, recipient, subject, body):
-    ''' send_mail '''
+def send_mail(smtp_server, sender, recipients, subject, body):
     mmsg = MIMEText(body, 'html')
     mmsg['Subject'] = subject
     mmsg['From'] = sender
-    mmsg['To'] = recipient
+    mmsg['To'] = ", ".join(recipients)
 
     session = smtplib.SMTP(smtp_server)
-    session.sendmail(sender, [recipient], mmsg.as_string())
+    session.sendmail(sender, recipients, mmsg.as_string())
     session.quit()
 
-def monitor_path(path, quota, conninfo, creds, smtp_server, sender, recipient):
-    ''' monitor_path '''
-    node = qumulo.rest.fs.read_dir_aggregates(conninfo, creds, path)
-    quota = int(quota) * GIGABYTE
-    subject = "Quota exceeded"
+def build_mail(nfspath, quota, current_usage, smtp_server, sender, recipients):
+    sane_current_usage = float(current_usage) / float(TERABYTE)
+    subject = storagename + "Quota exceeded"
     body = ""
+    body += "The usage on {} has exceeded the quota.<br>".format(nfspath)
+    body += "Current usage: %0.2f TB<br>" %sane_current_usage 
+    body += "Quota: %0.2f TB<br>" %quota
+    body += "<br>"
+    send_mail(smtp_server, sender, recipients, subject, body)
+        
+# Build email and check against quota for notification, return current usage for tracking
+def monitor_path(path, conninfo, creds):
+    try:
+        node = qumulo.rest.fs.read_dir_aggregates(conninfo, creds, path)
+    except Exception, excpt:
+        print 'Error retrieving path: %s' % excpt
+    else:
+        current_usage = int(node[0]['total_capacity'])
+        return current_usage
 
-    current_usage = int(node[0]['total_capacity'])
-    if current_usage > quota:
-        excess_usage = abs(quota - current_usage)
-        body += "Your usage on " + path + " exceeded your quota. "
-        body += "Current usage is " + str(current_usage) + \
-               ", while the quota specified is " + str(quota)
-        body += ". You exceeded the quota by: " + str("{:.2f}".format(excess_usage) + " bytes.")
-        body += "<br>"
-
-    for n in node[0]['files']:
-        if n['type'] == 'FS_FILE_TYPE_DIRECTORY':
-            current_usage = int(n['capacity_usage'])
-            print n['name']
-            if current_usage >= quota:
-                excess_usage = abs(quota - current_usage)
-                body += "Your usage on " + path + "/" + n['name'].encode('utf-8') + " exceeded your quota. "
-                body += "Current usage is " + str(current_usage) + \
-                       ", while the quota specified is " + str(quota)
-                body += ". You exceeded the quota by: " + str("{:.2f}".format(excess_usage) + " bytes.")
-                body += "<br>"
-
-    if body:
-        send_mail(smtp_server, sender, recipient,  subject, body)
-
+def build_csv(quotaname, current_usage, logfile):
+    with open(logfile, "a") as file:
+        file.write(quotaname + ',' + str(current_usage) + '\n')
+        
 ### Main subroutine
 def main(argv):
+    # Get credentials
+    (conninfo, creds) = login(host, user, password, port)
+    
+    # Overwrite log file
+    with open(logfile, "w") as file:
+        file.write(header + '\n')
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', dest='host', required=True)
-    parser.add_argument('--user', dest='user', required=True)
-    parser.add_argument('--password', dest='password', required=True)
-    parser.add_argument('--port', dest='port', default=8000, type=int)
-    parser.add_argument('--path', dest='path', default='/')
-    parser.add_argument('--quota', dest='quota', required=True, type=float)
-    parser.add_argument('--smtp', dest='smtp', default='mail.corp.qumulo.com')
-    parser.add_argument('--sender', dest='sender', default='qdf@qumulo.com')
-    parser.add_argument('--recipient', dest='recipient', default='karim@qumulo.com')
-
-    opts = parser.parse_args(argv)
-    (conninfo, creds) = login(opts.host, opts.user, opts.password, opts.port)
-    monitor_path(opts.path, opts.quota, conninfo, creds, opts.smtp,
-                 opts.sender, opts.recipient)
+    # Get quotas and generate CSV
+    for quotaname in quota_dict.keys():
+        path, nfspath, quota = quota_dict[quotaname]
+        current_usage = monitor_path(path, conninfo, creds)
+        if current_usage is not None:
+            quotaraw = int(quota) * TERABYTE
+            if current_usage > quotaraw:
+                build_mail(nfspath, quota, current_usage, smtp_server, sender, recipients)
+            build_csv(quotaname, current_usage, logfile)    
 
 # Main
 if __name__ == '__main__':
